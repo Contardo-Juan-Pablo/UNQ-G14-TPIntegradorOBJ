d343463303b8f9145853ca2623b51888b60243a6
Juan Pablo Contardo <contardo.juanpablo@gmail.com>
2020-12-02 16:22:17 -0300

    Se soluciono el problema con mock

--- test/AppTest/AppSEMTest.java
+++ test/AppTest/AppSEMTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
-
 import app.AppSEM;
 import app.Estado;
 import app.Modo;
